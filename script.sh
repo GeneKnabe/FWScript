6c4dcf2f8c26355e551da058ecd48fc988611eff
GeneKnabe <75308615+GeneKnabe@users.noreply.github.com>
2021-01-08 10:05:08 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#/usr/sbin/iptables -t nat -A POSTROUTING -o ens18 -j MASQUERADE
-#!/bin/bash
 
 echo "Checking the permission level"
 if [ "$EUID" -ne 0 ]
@@ -12,68 +10,90 @@ echo "Setup variables"
 
 #Pfade
 IPTABLES=/usr/sbin/iptables
-#Router
-ROUTEREXT=192.168.200.213/24
-ROUTERINT=172.16.13.1/24
-INTERFACEEXT=ens33
-INTERFACEINT=ens37
+#Router Interfaces und Addressen
+ROUTEREXT=192.168.0.53/24
+ROUTERINT=10.10.0.1/24
+INTERFACEEXT=ens18
+INTERFACEINT=ens19
 #Subnetze
-NETEXT=172.16.13.0
-NETINT=10.0.13.0
-NETALL=0.0.0.0
-#Internal IPs for SSH (EXT -> INT Network)
-SSHFORWARDIP=('' '' '' '')
-SSHFORWARDPORTFROMEXT=('8022' '9023' '9024' '9025')
-#Ports 
-FORWARDPORTS=('123' '37' '53' '80' '' '' '' '' '')
-INPUTPORTS=('22' '' '' '' '')
+NETEXT=192.168.0.0/24
+NETINT=10.10.0.0/24
+NETALL=0.0.0.0/0
+#Internal IPs welche via SSH von außen erreichbar sein sollten
+SSHFORWARDIP=('10.10.0.15')
+SSHFORWARDPORTFROMEXT=('8022')
+#Internal IPs welche via RDP von außen erreichbar sein sollten
+RDPFORWARDIP=('10.10.0.15')
+RDPFORWARDPORTFROMEXT=('8023')
+#TCP-Ports Welche freigegeben werden sollen (für das ganze netz)
+TCPFORWARDPORTS=('80' '443')
+#Ports Welche freigegeben werden sollen (für den Router)
+INPUTPORTS=('22')
+#Server für DNS ACCESS nach draußen
+DNSSERVERS=('10.10.0.15')
+#Server für Zeit-Server Zugriff
+NTPSERVERS=('10.10.0.15')
 
 case "$1" in 
 
 #Start Parameter Start
 start)
 
+#Flush$IPTABLES
 $IPTABLES -F
 $IPTABLES -F -t nat
+
 #NAT+FORWARDING
 $IPTABLES -t nat -A POSTROUTING -o $INTERFACEEXT -j MASQUERADE
-#$IPTABLES -A PREROUTING -t nat -i ens33 -p tcp --dport 80 -j DNAT --to 172.16.13.2:80
-#$IPTABLES -A FORWARD -p tcp -d $IPADMINDMZ --dport 80 -j ACCEPT
-#SSH ACCESS
-for i in SSHFORWARDIP; do
-    for j in SSHFORWARDPORTFROMEXT; do
-        $IPTABLES -t NAT -A REROUTING -p tcp -m tcp -i $INTERFACEEXT --dport $j -j DNAT --to-destination $i:22 
-    done
+
+#SSH ACCESS to Internal Clients from external
+j=0
+for i in $SSHFORWARDIP; do
+    $IPTABLES -t nat -A PREROUTING -p tcp -m tcp -i $INTERFACEEXT --dport ${SSHFORWARDPORTFROMEXT[$j]} -j DNAT --to-destination $i:22 
+    j=$((j + 1))
 done
-$IPTABLES --insert INPUT 1 -p tcp -s $NETALL -i $INTERFACEEXT --dport 22 -i $INTERFACEINT -j ACCEPT
-$IPTABLES --insert INPUT 1 -p tcp -s $IPADMINDMZ --dport 22 -i ens37 -j ACCEPT
-$IPTABLES --insert OUTPUT 1 -p tcp --sport 22 -o ens37 -j ACCEPT
-$IPTABLES --insert FORWARD 1 -p tcp -m tcp --sport 22 -o ens37 -j ACCEPT
-$IPTABLES --insert FORWARD 1 -p tcp -m tcp --sport 22 -o ens37 -j ACCEPT
-#ICMP ACCESS
-#$IPTABLES --insert INPUT 1 -p ICMP -j ACCEPT
-#$IPTABLES --insert OUTPUT 1 -p ICMP -j ACCEPT
-#$IPTABLES --insert FORWARD 1 -p ICMP -j ACCEPT
+
+#SSH for Router
+$IPTABLES --insert INPUT -p tcp -i $INTERFACEEXT --dport 22 -j ACCEPT
+$IPTABLES --insert INPUT -p tcp -i $INTERFACEEXT --sport 22 -j ACCEPT
+$IPTABLES --insert OUTPUT -p tcp -o $INTERFACEEXT --sport 22  -j ACCEPT
+$IPTABLES --insert OUTPUT -p tcp -o $INTERFACEEXT --dport 22  -j ACCEPT
+
+#DNS
+for i in $DNSSERVERS; do
+    $IPTABLES --insert FORWARD -p tcp -s $i --dport 53 -j ACCEPT
+    $IPTABLES --insert FORWARD -p tcp -d $i --sport 53 -j ACCEPT
+    $IPTABLES --insert FORWARD -p udp -d $i --sport 53 -j ACCEPT
+    $IPTABLES --insert FORWARD -p udp -s $i --dport 53 -j ACCEPT
+done
+
 #NTP
-#$IPTABLES --insert FORWARD 1 -p UDP -o ens37 --dport 123 -j ACCEPT
-#$IPTABLES --insert FORWARD 1 -p UDP -o ens33 --sport 123 -j ACCEPT
-#Webserver
-#$IPTABLES --insert FORWARD 1 -p tcp -d 172.16.13.3 --dport 80 -j ACCEPT
-#User WebACCESS
-#$IPTABLES --insert FORWARD 1 -p tcp -s 10.0.13.0 --dport 80 -j ACCEPT
-#$IPTABLES --insert FORWARD 1 -p tcp -d 10.0.13.0 --sport 80 -j ACCEPT
-#
-#$IPTABLES --insert FORWARD 1 -p tcp -s $IPADMINDMZ --dport 37 -j ACCEPT
-#$IPTABLES --insert FORWARD 1 -p UDP -s $IPADMINDMZ --dport 37 -j ACCEPT
-#$IPTABLES --insert INPUT 1 -p tcp -s $IPADMINDMZ --dport 37 -j ACCEPT
-#$IPTABLES --insert INPUT 1 -p UDP -s $IPADMINDMZ --dport 37 -j ACCEPT
+for i in $NTPSERVERS; do
+    $IPTABLES --insert FORWARD -p UDP -i $INTERFACEINT -s $i --sport 123 -o $INTERFACEEXT --dport 123 -j ACCEPT
+    $IPTABLES --insert FORWARD -p UDP -i $INTERFACEEXT --sport 123 -o $INTERFACEINT -d $i --dport 123 -j ACCEPT
+done
+
+#Internal Networkwide TCP Rules
+for i in $TCPFORWARDPORTS; do
+    $IPTABLES --insert FORWARD -p TCP -i $INTERFACEINT -s $NETINT --dport $i -j ACCEPT
+    $IPTABLES --insert FORWARD -p TCP -i $INTERFACEEXT --sport $i -d $NETINT -j ACCEPT
+done
+
+#RDP ACCESS
+j=0
+for i in $RDPFORWARDIP; do
+    $IPTABLES -t nat -A PREROUTING -p tcp -m tcp -i $INTERFACEEXT --dport ${RDPFORWARDPORTFROMEXT[$j]} -j DNAT --to-destination $i:3389
+    j=$((j + 1))
+done
 
 $IPTABLES -P INPUT DROP
 $IPTABLES -P FORWARD DROP
 $IPTABLES -P OUTPUT DROP
 
+#Save Rules
+/usr/sbin/iptables-save > /etc/iptables/rules.v4
 
-
+#Display set rules
 $IPTABLES -L -v -n
 
 
@@ -84,18 +104,20 @@ echo "Firewall on fire"
 #Start Parameter Stop
 stop)
 
+#Flush Tables which were modified
 $IPTABLES -F
 $IPTABLES -F -t nat
+
+#NAT+FORWARDING
+$IPTABLES -t nat -A POSTROUTING -o $INTERFACEEXT -j MASQUERADE
+
+#Set everything to Accept
 $IPTABLES -P INPUT ACCEPT
 $IPTABLES -P FORWARD ACCEPT
 $IPTABLES -P OUTPUT ACCEPT
-#NAT+FORWARDING
-#$IPTABLES -t nat -A POSTROUTING -o ens33 -j MASQUERADE
-#$IPTABLES -A PREROUTING -t nat -i ens33 -p tcp --dport 80 -j DNAT --to 172.16.13.2:80
-#$IPTABLES -A FORWARD -p tcp -d $IPADMINDMZ --dport 80 -j ACCEPT
-#Set Default
 
-
+#Save Rules
+/usr/sbin/iptables-save > /etc/iptables/rules.v4
 
 $IPTABLES -L -v -n
 
@@ -103,11 +125,20 @@ $IPTABLES -L -v -n
 echo "Firewall off (HE DED)"
 
 ;;
-esac
 
-#echo -ne '#####                     (33%)\r'
-#sleep 1
-#echo -ne '#############             (66%)\r'
-#sleep 1
-#echo -ne '#######################   (100%)\r'
-#echo -ne '\n'
+fullblock)
+
+echo "Wenn die SSH Verbindung abbricht hast du erfolgreich deinen eigenen Ast abgesägt"
+
+$IPTABLES -P INPUT DROP
+$IPTABLES -P FORWARD DROP
+$IPTABLES -P OUTPUT DROP
+
+#Save Rules
+/usr/sbin/iptables-save > /etc/iptables/rules.v4
+
+echo "Everything blocked"
+
+;;
+
+esac
